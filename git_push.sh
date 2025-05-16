f28b9ff9f925cd4e296771139f939c0b74518259
GitLab CI <valentino.lauciani@ingv.it>
2025-05-16 12:37:31 +0000

    Automated update from CI for commit: https://gitlab.rm.ingv.it/caravel/dante8/-/commit/38bbb300acf36bd8c523685d075c88bb39664875

--- git_push.sh
+++ git_push.sh
@@ -24,7 +24,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="First draft of Dante client"
+    release_note="API client"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
